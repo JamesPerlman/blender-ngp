434eb9479fdc8c49e9cc80440f0eafec1b680aa9
JamesPerlman <jam.e.perl@gmail.com>
2022-10-31 20:30:03 -0700

    fix masks (use custom props)

--- include/neural-graphics-primitives/mask_shapes.cuh
+++ include/neural-graphics-primitives/mask_shapes.cuh
@@ -40,31 +40,44 @@ enum class EMaskShape : int {
 
 // xyz -> yzx
 
-inline NGP_HOST_DEVICE float sdfBox(const Vector3f& p) {
-    Vector3f d = p.cwiseAbs() - Vector3f(1.0f, 1.0f, 1.0f);
+inline NGP_HOST_DEVICE float sdfBox(const Vector3f& p, const Vector3f& b) {
+    Vector3f d = p.cwiseAbs() - 0.5f * b;
     return d.cwiseMax(0.0f).norm() + min(max(d.x(), max(d.y(), d.z())), 0.0f);
 };
 
-inline NGP_HOST_DEVICE float sdfCylinder(const Vector3f& p) {
-    Vector2f d = Vector2f(Vector2f(p.y(), p.x()).norm(), p.z()).cwiseAbs() - Vector2f(1.0f, 1.0f);
+inline NGP_HOST_DEVICE float sdfCylinder(const Vector3f& p, const float& r, const float& h) {
+    Vector2f d = Vector2f(Vector2f(p.y(), p.x()).norm(), p.z()).cwiseAbs() - Vector2f(r, 0.5f * h);
     return d.cwiseMax(0.0f).norm() + min(max(d.x(), d.y()), 0.0f);
 };
 
-inline NGP_HOST_DEVICE float sdfSphere(const Vector3f& p) {
-    return p.norm() - 1.0f;
+inline NGP_HOST_DEVICE float sdfSphere(const Vector3f& p, const float& r) {
+    return p.norm() - r;
 };
 
 struct Mask3D {
     EMaskMode mode;
     EMaskShape shape;
     Matrix4f transform;
+    Vector4f config;
     float feather;
     float opacity;
 
-    NGP_HOST_DEVICE Mask3D(const EMaskMode& mode, const EMaskShape& shape, const Matrix4f& transform, const float& feather, const float& opacity)
-        : mode(mode), shape(shape), transform(transform), feather(feather), opacity(opacity) {};
+    NGP_HOST_DEVICE Mask3D(const EMaskShape& shape, const Matrix4f& transform, const EMaskMode& mode, const Vector4f& config, const float& feather, const float& opacity)
+        : mode(mode), shape(shape), transform(transform), config(config), feather(feather), opacity(opacity) {};
     
-    NGP_HOST_DEVICE Mask3D() : mode(EMaskMode::Add), shape(EMaskShape::Box), transform(Matrix4f::Identity()), feather(0.0f), opacity(0.0f) {};
+    NGP_HOST_DEVICE Mask3D() : mode(EMaskMode::Add), shape(EMaskShape::Box), transform(Matrix4f::Identity()), config(Vector4f::Zero()), feather(0.0f), opacity(0.0f) {};
+
+    NGP_HOST_DEVICE static Mask3D Box(const Vector3f& dims, const Matrix4f& transform, const EMaskMode& mode, const float& feather, const float& opacity) {
+        return Mask3D(EMaskShape::Box, transform, mode, Vector4f(dims.x(), dims.y(), dims.z(), 0.0f), feather, opacity);
+    };
+
+    NGP_HOST_DEVICE static Mask3D Cylinder(const float& radius, const float& height, const Matrix4f& transform, const EMaskMode& mode, const float& feather, const float& opacity) {
+        return Mask3D(EMaskShape::Cylinder, transform, mode, Vector4f(radius, height, 0.0f, 0.0f), feather, opacity);
+    };
+
+    NGP_HOST_DEVICE static Mask3D Sphere(const float& radius, const Matrix4f& transform, const EMaskMode& mode, const float& feather, const float& opacity) {
+        return Mask3D(EMaskShape::Sphere, transform, mode, Vector4f(radius, 0.0f, 0.0f, 0.0f), feather, opacity);
+    };
 
     inline NGP_HOST_DEVICE float sample(const Vector3f& p) const {
         
@@ -76,13 +89,13 @@ struct Mask3D {
         float d = 0.0f;
         switch (shape) {
             case EMaskShape::Box:
-                d = sdfBox(p_local);
+                d = sdfBox(p_local, config.head<3>());
                 break;
             case EMaskShape::Cylinder:
-                d = sdfCylinder(p_local);
+                d = sdfCylinder(p_local, config.x(), config.y());
                 break;
             case EMaskShape::Sphere:
-                d = sdfSphere(p_local);
+                d = sdfSphere(p_local, config.x());
                 break;
         }
 
